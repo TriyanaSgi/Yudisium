4115dcc4f44883e326c980a9e77da1c0f9fa24d7
lillysky07 <deliaputriadel096@gmail.com>
2024-06-04 08:55:19 +0700

    update db

--- app/Models/batch.php
+++ app/Models/batch.php
@@ -8,10 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class batch extends Model
 {
     use HasFactory;
-    
+
     protected $table = 'batch';
     protected $primaryKey = 'id_batch';
-    
+
     protected $fillable = [
         'id_batch',
         'nama_mhs',
