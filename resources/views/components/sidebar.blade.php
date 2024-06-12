35343f9b15526d5149d1d444ba579aa99e196258
son-alone <hasyimhusaini470@gmail.com>
2024-06-12 13:12:34 +0700

    ...

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -24,20 +24,20 @@
         @endif
 
             <!-- profile ganti password -->
-            @if (auth()->user()->role == 'superadmin' || auth()->user()->role == 'superadmin')
+            @if (auth()->user()->role == 'superadmin')
             <li class="menu-header">Profile</li>
             <li class="{{ Request::is('profile/edit') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ url('profile/edit') }}"><i class="far fa-user"></i> <span>Profile</span></a>
             </li>
             @endif
 
-            @if (auth()->user()->role == 'superadmin' || auth()->user()->role == 'superadmin')
+            @if (auth()->user()->role == 'superadmin')
             <li class="{{ Request::is('profile/change-password') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ url('profile/change-password') }}"><i class="fas fa-key"></i> <span>Ganti Password</span></a>
             </li>
             @endif
 
-            @if (auth()->user()->role == 'superadmin' || auth()->user()->role == 'superadmin')
+            @if (auth()->user()->role == 'superadmin')
             <li class="{{ Request::is('hakakses/') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ url('hakakses/') }}"><i class="fas fa-key"></i> <span>Hak Akses</span></a>
             </li>
@@ -51,19 +51,19 @@
             </li>
             @endif
 
-            @if (auth()->user()->role == 'superadmin' || auth()->user()->role == 'superadmin')
+            @if (auth()->user()->role == 'superadmin')
             <li class="{{ Request::is('mahasiswa') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ url('mahasiswa') }}"><i class="fas fa-university"></i> <span> Data Mahasiswa </span></a>
             </li>
             @endif
 
-            @if (auth()->user()->role == 'superadmin' || auth()->user()->role == 'superadmin')
+            @if (auth()->user()->role == 'superadmin')
             <li class="{{ Request::is('prodi') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ url('prodi') }}"><i class="fas fa-university"></i> <span> Data Prodi </span></a>
             </li>
             @endif
 
-            @if (auth()->user()->role == 'superadmin' || auth()->user()->role == 'superadmin')
+            @if (auth()->user()->role == 'superadmin')
             <li class="{{ Request::is('pt') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ url('pt') }}"><i class="fas fa-university"></i> <span> Data Perguruan Tinggi </span></a>
             </li>
